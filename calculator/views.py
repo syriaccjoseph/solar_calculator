5e68c094da2e8e23d2a79fed077c60e694f0d3f1
Ajith Joseph <syriaccjoseph@gmail.com>
2020-01-28 17:22:36 -0500

    added calculator template

--- calculator/views.py
+++ calculator/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request, area):
 
@@ -12,14 +13,17 @@ def index(request, area):
     # r = solar panel yield or efficiency(%) 
     # H = Annual average solar radiation on tilted panels (shadings not included)
     # PR = Performance ratio, coefficient for losses (range between 0.5 and 0.9, default value = 0.75)
-    
     a = float(area)
     r = 15.0
     h = 1361.0
     pr = 0.75
-
     e = a * r * h * pr
 
-    return HttpResponse("Nominal power : " + str(e))
+    template = loader.get_template('calculator/index.html')
+    context = {
+        'nominal_power': e,
+    }
+
+    return HttpResponse(template.render(context, request))
 
 # Create your views here.
