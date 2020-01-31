8e5897a4accd1a41bc0d03eaf5f2bfbfa70c6c7c
Ajith Joseph <syriaccjoseph@gmail.com>
2020-01-31 17:51:28 -0500

    added tests for calculator app

--- calculator/tests.py
+++ calculator/tests.py
@@ -1,6 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+class CalculatorTest(TestCase):
+    def setUp(self):
+        client = Client()
+    
+    def test_redirect(self):
+        response = self.client.get('/')
+        self.assertRedirects(response, '/calculator/')
+
+    def test_calculator_status(self):
+        response = self.client.get('/calculator/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_calculator_context(self):
+        response = self.client.post('/calculator/', {'area': 12})
+        self.assertEqual(response.context['area'], '12.0')
+        self.assertEqual(response.context['nominal_power'], '183735.0')
+
+
